ef33478acb7ed532fae7aa9464d29bde7d0afb8e
luisOtavio15 <ol@roeor.com>
2026-03-19 07:51:21 -0300

    add impl Quicksort revert

--- src/Quicksort.java
+++ src/Quicksort.java
@@ -2,8 +2,9 @@ public class Quicksort {
     public static void main(String[] args) throws Exception {
       int[] vetor = gerarVetor(10);
         printar(vetor);
-        quicksort(vetor, 0, vetor.length-1);
+        quicksortRevert(vetor, 0, vetor.length-1);
         printar(vetor);
+
     
     }
     public static int[] gerarVetor(int n) {
@@ -27,6 +28,33 @@ public class Quicksort {
             quicksort(vetor, inicio, pivo - 1);
             quicksort(vetor, pivo + 1, fim);
         }
+    }
+
+    static void quicksortRevert(int[] vetor, int inicio, int fim){
+        if(inicio < fim){
+            int pivo = particionarR(vetor, inicio, fim);
+            quicksortRevert(vetor, inicio, pivo-1);
+            quicksortRevert(vetor, pivo+1, fim);
+        }
+    }
+
+    public static int particionarR(int[] vetor, int left, int right){
+        int i = (left -1);
+        int pivo = right;
+
+        for (int j = left; j < right; j++) {
+            if(vetor[j] > vetor[pivo]){
+                i++;
+                int aux = vetor[j];
+                vetor[j] = vetor[i];
+                vetor[i] = aux;
+            }
+        }
+        i++;
+        int aux = vetor[right];
+        vetor[right] = vetor[i];
+        vetor[i] = aux;
+        return i;
     }
     public static int particionar(int[] vetor, int left, int right){
         int i = left;
